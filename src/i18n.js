a4c32cad8884a702af1aeb91c9f85ff07ee687e5
Artur Yeszhanov <arturyeszhanov@gmail.com>
2025-06-15 13:14:13 +0500

    Реализована валидация формы и подключён i18n

---
+++ src/i18n.js
@@ -0,0 +1,12 @@
+import i18next from 'i18next';
+import ru from './locales/ru.js';
+
+const initI18n = () => i18next.init({
+  lng: 'ru',
+  debug: true,
+  resources: {
+    ru,
+  },
+});
+
+export default initI18n;
